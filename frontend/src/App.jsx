b66e23cd5016bb537c0c21d7b14949a4934c336e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-18 22:19:08 +0100

    Add React and Vite setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
