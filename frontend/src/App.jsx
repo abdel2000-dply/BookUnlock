d1596dd4699c3d564c40d2b571caf8c0320e7545
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-22 18:55:59 +0100

    Add Header component and import CSS files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
+// Desc: Main App component
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/common/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
+// Importing the CSS file
+import './assets/Styles/Header.css';
+import './index.css';
+
 function App() {
   return(
     <Router>
+      <Header />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/login" element={<Login />} />
