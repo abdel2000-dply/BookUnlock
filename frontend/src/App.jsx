d0c79189b5e65b1d583cacfdb3619b44d0ae8172
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-23 22:49:39 +0100

    Add Book page to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/common/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Book from './pages/Book';
 
 // Importing the CSS file
 import './assets/Styles/Header.css';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path='/book' element={<Book />} />
       </Routes>
     </Router>
   );
