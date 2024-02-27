bf5bc090e5fe6f6da4ab3386dfbf85e1ba9be76f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-27 15:27:28 +0100

    including the topbooks on 2023 section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../assets/Styles/Home.css';
+import "../assets/Styles/Home.css";
+import TopBooks from "../components/TopBooks";
 
 function Home() {
   return (
     <main>
+      {/*----------------------------------------------------------------- Hero section */}
       <section className="hero">
         <div className="hero-body">
           <div className="hero-content">
             <h1 className="title">Welcome to BookUnluck</h1>
             <h2 className="subtitle">The best place to find books</h2>
-            <Link
-              to="/signup"
-              className="button"
-            >
+            <Link to="/signup" className="button">
               Get Started
             </Link>
           </div>
         </div>
       </section>
-      <section>
-        
+      {/*------------------------------------------------------------------ Newest books section */}
+      <section className="newest-books">
+        <h2>Top 10 Books in 2023</h2>
+        <TopBooks />
       </section>
     </main>
   );
