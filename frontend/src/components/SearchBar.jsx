2389e2b7293bfd1a16deb7ffb6589758eee6a5c2
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-02 01:43:36 +0100

    Add search icons and handle enter key press in SearchBar component

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Select from "react-select";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = ({ options, searchTitle, setSearchTitle, searchAuthor, setSearchAuthor, selectedCategory, setSelectedCategory, handleSearch }) => {
     
@@ -7,29 +9,42 @@ const SearchBar = ({ options, searchTitle, setSearchTitle, searchAuthor, setSear
     const handleCategoryChange = (selectedOption) => {
         setSelectedCategory(selectedOption);
     };
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            handleSearch();
+        }
+    }
 
     return (
         <div className="search-bar-container">
-            <input
-                className="search-bar bytitle"
-                type="text"
-                placeholder="Search for books by title ..."
-                value={searchTitle}
-                onChange={(e) => setSearchTitle(e.target.value)}
-            />
-            <input
-                className="search-bar byauthor"
-                type="text"
-                placeholder="Search for books by Author ..."
-                value={searchAuthor}
-                onChange={(e) => setSearchAuthor(e.target.value)}
-            />
+            <div className="search-input-container bytitle">
+                <input
+                    className="search-bar"
+                    type="text"
+                    placeholder="Search by title ..."
+                    value={searchTitle}
+                    onChange={(e) => setSearchTitle(e.target.value)}
+                    onKeyPress={handleKeyPress}
+                />
+                <FontAwesomeIcon icon={faSearch} className="search-icon" />
+            </div>
+            <div className="search-input-container byauthor">
+                <input
+                    className="search-bar"
+                    type="text"
+                    placeholder="Search by Author ..."
+                    value={searchAuthor}
+                    onChange={(e) => setSearchAuthor(e.target.value)}
+                    onKeyPress={handleKeyPress}
+                />
+                <FontAwesomeIcon icon={faSearch} className="search-icon" />
+            </div>
             {/* category dropdown */}
             <Select
                 className="categories"
                 options={options}
                 isMulti
-                placeholder="Select categories ..."
+                placeholder="Categories ..."
                 value={selectedCategory}
                 onChange={handleCategoryChange}
             />
