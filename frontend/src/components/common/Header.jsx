656df492106333a3ba4ffa14358d7cfaa3982a3a
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-04 00:00:15 +0100

    Fix logo file name and update header behavior

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,51 +1,59 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import Logo from "../../assets/images/book-unluck-logo.png";
-
+import Logo from "../../assets/images/bookunluck-logo.png";
 function Header() {
-    //-----------------Sticky header-----------------
-    // const [isSticky, setIsSticky] = useState(false);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [headerShow, setHeaderShow] = useState(true);
 
-    // const handleScroll = () => {
-    // const scrollTop = window.scrollY;
-    // const headerHeight = document.querySelector('header').offsetHeight; // Get header's height
+  useEffect(() => {
+    const handleScroll = () => {
+      let st = window.pageYOffset || document.documentElement.scrollTop;
+      if (st > lastScrollTop) {
+        setHeaderShow(false);
+      } else {
+        setHeaderShow(true);
+      }
+      setLastScrollTop(st <= 0 ? 0 : st);
+    };
 
-    // if (scrollTop > headerHeight) {
-    //     setIsSticky(true);
-    // } else {
-    //     setIsSticky(false);
-    // }
-    // };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollTop]);
 
-    // useEffect(() => {
-    // window.addEventListener('scroll', handleScroll);
-
-    // return () => window.removeEventListener('scroll', handleScroll);
-    // }, []);
-
-    return (
-        // <header className={isSticky ? 'sticky' : ''}>
-        <header>
-            <nav className="navbar">
-                <div className="navbar-brand">
-                    <a href="/">
-                        <img src={Logo} alt="bookunluck" className="logo"/>
-                    </a>
-                </div>
-                <div className="container">
-                    <div className="navbar-menu">
-                        <a className="navbar-item" href="/">Home</a>
-                        <a className="navbar-item" href="/explore">Explore</a>
-                        <a className="navbar-item" href="/">About us</a>
-                    </div>
-                    <div className="profile">
-                        <a className="navbar-item" href="/signup">Sign up</a>
-                        <a className="navbar-item" href="/login">Log in</a>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={headerShow ? "header-show" : "header-hide"}>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <a href="/">
+            <img src={Logo} alt="bookunluck" className="logo" />
+          </a>
+        </div>
+        <div className="container">
+          <div className="navbar-menu">
+            <a className="navbar-item" href="/">
+              Home
+            </a>
+            <a className="navbar-item" href="/explore">
+              Explore
+            </a>
+            <a className="navbar-item" href="/">
+              About us
+            </a>
+          </div>
+          <div className="profile">
+            <a className="navbar-item user" href="/signup">
+              Sign up
+            </a>
+            <a className="navbar-item user" href="/login">
+              Log in
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
