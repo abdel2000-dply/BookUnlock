14bbb9530f3284cba47ca6280a096398c6fef80a
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-03 18:35:43 +0100

    Add Footer component

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <p>&copy; 2022 BookUnluck. All rights reserved.</p>
+                <ul className="social-icons">
+                    <li>
+                        <a href="https://twitter.com/bookunluck" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/bookunluck" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/bookunluck" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
