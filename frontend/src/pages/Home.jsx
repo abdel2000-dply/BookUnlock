2961679c43e98b5fa7d322ead26db60c67952aa2
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-03 18:37:06 +0100

    Add about section to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,6 +23,32 @@ function Home() {
         <h2>Top 10 Books in 2023</h2>
         <TopBooks />
       </section>
+      <section className="about">
+        <h2>About BookUnluck</h2>
+        <img></img>
+        <p>
+          BookUnluck is a platform where you can find the best books to read. We
+          have a wide variety of books from different genres. You can find books
+          from different authors and publishers. You can also find books in
+          different languages. 
+          We have a recommendation system that will help
+          you find the best books based on your interests. You can also find
+          books based on their popularity and ratings. You can also find books
+          based on their publication date. You can also find books based on
+          their page count. You can also find books based on their categories.
+          You can also find books based on their average rating. You can also
+          find books based on their rating count. You can also find books based
+          on their preview link. You can also find books based on their image
+          links. You can also find books based on their authors. You can also
+          find books based on their published date.You can also find books based
+          on their categories. You can also find books based on their authors.
+        </p>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+      </section>
     </main>
   );
 }
