a1da44b739f9b28e17cbae021183b82191b9427f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-03 18:35:12 +0100

    Add link to book details page in BookCard component

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/Styles/BookCard.css";
 
 function BookCard({ book }) {
   if (!book) {
@@ -6,20 +8,22 @@ function BookCard({ book }) {
   }
   return (
     <div className="book-card">
-      <div className="book-card-image">
-        {book.volumeInfo.imageLinks && (
-          <img
-            src={book.volumeInfo.imageLinks.thumbnail.replace(
-              "zoom=1",
-              "zoom=3"
-            )}
-            alt={book.volumeInfo.title}
-          />
-        )}
-        <div className="book-card-overlay">
-          <h4 className="book-card-title">{book.volumeInfo.title}</h4>
+      <Link to={{ pathname: `/books/${book.id}`, state: { book } }}>
+        <div className="book-card-image">
+          {book.volumeInfo.imageLinks && (
+            <img
+              src={book.volumeInfo.imageLinks.thumbnail.replace(
+                "zoom=1",
+                "zoom=3"
+              )}
+              alt={book.volumeInfo.title}
+            />
+          )}
+          <div className="book-card-overlay">
+            <h4 className="book-card-title">{book.volumeInfo.title}</h4>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
