46277a40fb566b316b1e523cb0a129b6fa6d290e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-04 18:27:57 +0100

    Add hero image and open book icon to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HeroImage from "../assets/images/reading-a-book-illustration.png";
 import "../assets/Styles/Home.css";
 import TopBooks from "../components/TopBooks";
+import OpenBook from "../assets/images/open-book.png";
 
 function Home() {
   return (
@@ -9,12 +11,16 @@ function Home() {
       {/*----------------------------------------------------------------- Hero section */}
       <section className="hero">
         <div className="hero-body">
-          <div className="hero-content">
-            <h1 className="title">Welcome to BookUnluck</h1>
-            <h2 className="subtitle">The best place to find books</h2>
-            <Link to="/signup" className="button">
-              Get Started
+          <div className="hero-text-content">
+            <h1 className="hero-title">Welcome to BookUnluck</h1>
+            <h2 className="hero-subtitle">The best place to find books</h2>
+            <Link to="/signup" className="call-to-action">
+              <img src={OpenBook} alt="open book" className="open-book-icon" />
+              Explore
             </Link>
+          </div>
+          <div className="hero-image-content">
+            <img src={HeroImage} alt="Hero image" />
           </div>
         </div>
       </section>
@@ -30,8 +36,7 @@ function Home() {
           BookUnluck is a platform where you can find the best books to read. We
           have a wide variety of books from different genres. You can find books
           from different authors and publishers. You can also find books in
-          different languages. 
-          We have a recommendation system that will help
+          different languages. We have a recommendation system that will help
           you find the best books based on your interests. You can also find
           books based on their popularity and ratings. You can also find books
           based on their publication date. You can also find books based on
@@ -47,7 +52,6 @@ function Home() {
         <br></br>
         <br></br>
         <br></br>
-        
       </section>
     </main>
   );
