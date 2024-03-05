771f35ef2dcc6c85e382433f470e3a3553abc68c
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-05 16:26:23 +0100

    Refactor Explore page and explore.css, handle paginations

--- frontend/src/pages/Explore.jsx
+++ frontend/src/pages/Explore.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BookCard from "../components/BookCard";
 import SearchBar from "../components/SearchBar";
 import "../assets/Styles/BookCard.css";
@@ -41,11 +41,13 @@ function Explore() {
     { value: "cookbooks", label: "Cookbooks" },
     { value: "ebooks", label: "Ebooks" },
     { value: "graphicnovels", label: "Graphic Novels" },
-    { value: "chicklit", label: "Chick Lit" },
+    { value: "chicklit", label: "Chick Lit" }
     // ... you can add more categories here if needed
   ];
 
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchClicked, setSearchClicked] = useState(false);
 
   const apiKey = "AIzaSyCvF51voi8E3lwf44fOIkQ75VwWJXkzuVk";
 
@@ -56,19 +58,25 @@ function Explore() {
     try {
       let url = "https://www.googleapis.com/books/v1/volumes?q=";
 
-      if (searchTitle) {
-        url += `intitle:${encodeURIComponent(searchTitle)}`;
+      // If there are no search parameters, fetch random books
+      if (!searchTitle && !searchAuthor && !selectedCategory) {
+        url += `orderBy=newest&maxResults=10&key=${apiKey}&language=en`;
+      } else {
+        if (searchTitle) {
+          url += `intitle:${encodeURIComponent(searchTitle)}`;
+        }
+        if (searchAuthor) {
+          url += `+inauthor:${encodeURIComponent(searchAuthor)}`;
+        }
+        if (selectedCategory) {
+          selectedCategory.forEach((category) => {
+            url += `+subject:${encodeURIComponent(category.value)}`;
+          });
+        }
+        url += `&startIndex=${
+          (currentPage - 1) * 10
+        }&maxResults=10&orderBy=relevance&printType=books&key=${apiKey}&language=en`;
       }
-      if (searchAuthor) {
-        url += `+inauthor:${encodeURIComponent(searchAuthor)}`;
-      }
-      if (selectedCategory) {
-        selectedCategory.forEach((category) => {
-          url += `+subject:${encodeURIComponent(category.value)}`;
-        });
-      }
-
-      url += `&orderby=relevance&printType=books&key=${apiKey}&language=en`;
 
       const response = await fetch(url);
       const data = await response.json();
@@ -76,22 +84,38 @@ function Explore() {
       if (!data.items) {
         setError("No results found.");
         setBookData([]);
+        return;
       }
 
       const filteredBooks = data.items.filter(
         (book) => book.volumeInfo.pageCount
       );
       setBookData(filteredBooks);
-    // } catch (err) {
-    //   setError("Error fetching book data.");
+    } catch (err) {
+      setError("Error fetching book data.");
     } finally {
       setLoading(false); // Set loading to false when the fetch is complete
     }
   };
 
-  const handleSearch = () => {
+  // Add a function to go to the next page
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  // Add a function to go to the previous page
+  const goToPreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  useEffect(() => {
     fetchBooks();
-    setDisplay(true); // display the books after the search
+  }, [searchClicked, currentPage]);
+
+  const handleSearch = () => {
+    setCurrentPage(1); // Reset the currentPage to 1
+    setDisplay(true); // Display the books after the search
+    setSearchClicked((prev) => !prev); // Toggle searchClicked
   };
 
   if (loading) {
@@ -100,28 +124,35 @@ function Explore() {
 
   return (
     <div className="explore-page">
-    <div className="explore-container">
-      <h2 className="search-heading">Find new books to read</h2>
-      <SearchBar
-        options={options}
-        searchTitle={searchTitle}
-        setSearchTitle={setSearchTitle}
-        searchAuthor={searchAuthor}
-        setSearchAuthor={setSearchAuthor}
-        selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}
-        handleSearch={handleSearch}
-      />
-      {error && <div className="error">{error}</div>}
-      <div className="books-container">
-        {bookData &&
-          bookData.map(
-            (book) => (
-              console.log(book.id), (<BookCard book={book} key={book.id} />)
-            )
-          )}
+      <div className="e-s-container">
+        <SearchBar
+          options={options}
+          searchTitle={searchTitle}
+          setSearchTitle={setSearchTitle}
+          searchAuthor={searchAuthor}
+          setSearchAuthor={setSearchAuthor}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          handleSearch={handleSearch}
+        />
       </div>
-    </div>
+      <div className="results">
+        {error && <div className="error">{error}</div>}
+        <div className="books-container">
+          {bookData &&
+            bookData.map(
+              (book) => (
+                console.log(book.id), (<BookCard book={book} key={book.id} />)
+              )
+            )}
+        </div>
+        <div className="pagination">
+          {currentPage > 1 && (
+            <button onClick={goToPreviousPage}>Previous</button>
+          )}
+          {display && <button onClick={goToNextPage}>Next</button>}
+        </div>
+      </div>
     </div>
   );
 }
