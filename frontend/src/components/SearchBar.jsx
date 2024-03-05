0802777bbd3456340c5230978792e5c7034eb3a6
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-05 16:32:03 +0100

    Refactor SearchBar component

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,58 +1,68 @@
-import React from 'react';
+import React from "react";
 import Select from "react-select";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const SearchBar = ({ options, searchTitle, setSearchTitle, searchAuthor, setSearchAuthor, selectedCategory, setSelectedCategory, handleSearch }) => {
-    
-    
-    const handleCategoryChange = (selectedOption) => {
-        setSelectedCategory(selectedOption);
-    };
-    const handleKeyPress = (e) => {
-        if (e.key === "Enter") {
-            handleSearch();
-        }
+const SearchBar = ({
+  options,
+  searchTitle,
+  setSearchTitle,
+  searchAuthor,
+  setSearchAuthor,
+  selectedCategory,
+  setSelectedCategory,
+  handleSearch
+}) => {
+  const handleCategoryChange = (selectedOption) => {
+    setSelectedCategory(selectedOption);
+  };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
     }
+  };
 
-    return (
-        <div className="search-bar-container">
-            <div className="search-input-container bytitle">
-                <input
-                    className="search-bar"
-                    type="text"
-                    placeholder="Search by title ..."
-                    value={searchTitle}
-                    onChange={(e) => setSearchTitle(e.target.value)}
-                    onKeyPress={handleKeyPress}
-                />
-                <FontAwesomeIcon icon={faSearch} className="search-icon" />
-            </div>
-            <div className="search-input-container byauthor">
-                <input
-                    className="search-bar"
-                    type="text"
-                    placeholder="Search by Author ..."
-                    value={searchAuthor}
-                    onChange={(e) => setSearchAuthor(e.target.value)}
-                    onKeyPress={handleKeyPress}
-                />
-                <FontAwesomeIcon icon={faSearch} className="search-icon" />
-            </div>
-            {/* category dropdown */}
-            <Select
-                className="categories"
-                options={options}
-                isMulti
-                placeholder="Categories ..."
-                value={selectedCategory}
-                onChange={handleCategoryChange}
-            />
-            <button className="search-button" onClick={handleSearch}>
-                Search
-            </button>
+  return (
+    <div className="explore-bar">
+      <h2 className="search-heading">Books</h2>
+      <div className="search-bar-container">
+        <div className="search-input-container bytitle">
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Search by title ..."
+            value={searchTitle}
+            onChange={(e) => setSearchTitle(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+          <FontAwesomeIcon icon={faSearch} className="search-icon" />
         </div>
-    );
+        <div className="search-input-container byauthor">
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Search by Author ..."
+            value={searchAuthor}
+            onChange={(e) => setSearchAuthor(e.target.value)}
+            onKeyPress={handleKeyPress}
+          />
+          <FontAwesomeIcon icon={faSearch} className="search-icon" />
+        </div>
+        {/* category dropdown */}
+        <Select
+          className="categories"
+          options={options}
+          isMulti
+          placeholder="Categories ..."
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        />
+        <button className="search-button" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
