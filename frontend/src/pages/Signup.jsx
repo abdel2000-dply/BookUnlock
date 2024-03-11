f8bc039a85d1418908f87e03184e21f10a8ec73f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-11 20:53:00 +0000

    Add form functionality to Signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Import the CSS file
+import "../assets/Styles/Form.css";
 
 function Signup() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const url = "http://localhost:5000/api/v1/signup";
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, email, password }),
+        });
+
+        if (response.ok) {
+            alert("Signup successful");
+        } else {
+            console.log("Signup failed");
+        }
+    };
+
     return (
-        <div>
+        <div className="form-container">
         <h1>Signup</h1>
         <p>Signup for an account</p>
-        <form>
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username"></input><br></br>
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email"></input><br></br>
-            <label for="password">Password</label>
-            <input type="text" id="password" name="password"></input><br></br>
-            <input type="submit" value="Submit"></input>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" value={username} onChange={e => setUsername(e.target.value)} /><br />
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} /><br />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} /><br />
+            <input type="submit" value="Submit" />
         </form>
         </div>
     );
