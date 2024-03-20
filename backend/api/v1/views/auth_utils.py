15f7d920eb2ec6b559fec5898926ec358cba3877
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-03-20 00:25:55 +0000

    Add auth utils view for generating tokens

---
+++ backend/api/v1/views/auth_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""auth utils view"""
+
+from flask import current_app
+import jwt
+from datetime import datetime, timedelta
+from flask_login import current_user
+
+def generate_token(user):
+    expiration = datetime.utcnow() + timedelta(days=current_app.config['TOKEN_EXPIRATION_DAYS'])
+    token_data = {
+        'user_id': user.id,
+        'exp': expiration
+    }
+    return jwt.encode(token_data, current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
